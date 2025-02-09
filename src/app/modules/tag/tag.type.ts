003885e7151644cf0772e58baed8afc646e45497
Valentinmagde <valentinmagde@gmail.com>
2025-02-09 20:46:46 +0400

    update tag type file

--- src/app/modules/tag/tag.type.ts
+++ src/app/modules/tag/tag.type.ts
@@ -1,10 +1,10 @@
-import PostType from "../post/post.type";
+// import PostType from "../post/post.type";
 
 export default interface TagType {
   _id: string;
   ids: Array<string>;
   name: object;
   slug: object;
-  posts: Array<PostType>;
+  posts: Array<any>;
   status: "show" | "hide";
 }
